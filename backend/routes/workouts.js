2ad5361a3b600ce6c930341491101de77a53d22e
Dhanuja Thishakya <d.thishakya@gmail.com>
2023-11-12 13:12:23 +0530

    add get conttolers

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,30 +1,20 @@
 const express=require('express')
-const Workout=require('../models/Workouts')
+const {
+    createWorkout,
+    getWorkouts,
+    getWorkout
+}=require('../controllers/workoutController')
 
 const router= express.Router()
 
 // get all workout
-router.get('/', (req,res)=>{
-    res.json({mssg: "GET all workouts"})
-})
+router.get('/',getWorkouts)
 
 //get single workout
-router.get('/:id', (req,res)=>{
-    res.json({mssg: "GET a single workouts"})
-})
+router.get('/:id', getWorkout)
  
 // POST a new workout
-router.post('/', async (req,res)=>{
-    const {title,load,reps}=req.body
-
-    try{
-        const workout=await Workout.create({title,load,reps})
-        res.status(200).json(workout)
-    }catch(error){
-        res.status(400).json({error: error.message})
-    }
-    // res.json({mssg: "POST a new workout"})
-})
+router.post('/', createWorkout)
 
 // DELETE a workout
 router.delete('/:id', (req,res)=>{
